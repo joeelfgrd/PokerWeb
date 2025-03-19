5d9343b4743f46c52ebdd736e135e0000ae8cca7
Joel <joelfigueiridomolares@gmail.com>
2025-03-19 22:16:33 +0100

    First commit

---
+++ src/main/java/edu/badpals/pokerweb/PokerWebApplication.java
@@ -0,0 +1,13 @@
+package edu.badpals.pokerweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokerWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PokerWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/badpals/pokerweb/PokerWebApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.badpals.pokerweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokerWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
