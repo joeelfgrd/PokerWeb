3ddb10aa6f649d86e0f9c3b22c2fe0befaefbbdb
Joel <joelfigueiridomolares@gmail.com>
2025-03-25 21:34:01 +0100

    feat(UsuarioController):Terminada funcionalidad login

---
+++ src/main/java/edu/badpals/pokerweb/controller/UsuarioController.java
@@ -0,0 +1,46 @@
+package edu.badpals.pokerweb.controller;
+
+import edu.badpals.pokerweb.dtos.RegistroUsuarioDTO;
+import edu.badpals.pokerweb.dtos.LoginDTO;
+import edu.badpals.pokerweb.dtos.UsuarioLogueadoDTO;
+import edu.badpals.pokerweb.service.UsuarioService;
+import edu.badpals.pokerweb.service.UsuarioService.LoginStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/usuarios")
+public class UsuarioController {
+
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @PostMapping("/registro")
+    public ResponseEntity<UsuarioLogueadoDTO> registrar(@RequestBody RegistroUsuarioDTO dto) {
+        try {
+            UsuarioLogueadoDTO usuarioRegistrado = usuarioService.registrar(dto);
+            return ResponseEntity.ok(usuarioRegistrado);
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<UsuarioLogueadoDTO> login(@RequestBody LoginDTO dto) {
+        LoginStatus status = usuarioService.login(dto);
+
+        if (status == LoginStatus.LOGIN_OK) {
+            UsuarioLogueadoDTO usuario = usuarioService.getUsuarioResponseByEmail(dto.getEmail());
+            return ResponseEntity.ok(usuario);
+        } else if (status == LoginStatus.USER_NOT_FOUND) {
+            return ResponseEntity.status(404).body(null);
+        } else if (status == LoginStatus.ERROR_PASSWORD) {
+            return ResponseEntity.status(401).body(null);
+        } else if (status == LoginStatus.USER_BLOCKED) {
+            return ResponseEntity.status(403).body(null);
+        } else {
+            return ResponseEntity.status(500).build();
+        }
+    }
+}
--- src/main/java/edu/badpals/pokerweb/repository/UsuarioRepository.java
+++ src/main/java/edu/badpals/pokerweb/repository/UsuarioRepository.java
@@ -1,6 +1,8 @@
 package edu.badpals.pokerweb.repository;
 
 import edu.badpals.pokerweb.model.Usuario;
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
@@ -8,3 +10,5 @@ import java.util.Optional;
 public interface UsuarioRepository extends CrudRepository<Usuario, Long> {
     Optional<Usuario> findByEmail(String s);
 }
+
+
