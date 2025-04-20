af89edcf270cded9890ac5932d4b36015b9c99de
Joel <joelfigueiridomolares@gmail.com>
2025-04-21 01:23:33 +0200

    Doc:Añadir comentarios a metodos de evaluacion de manos

--- src/main/java/edu/badpals/pokerweb/domain/services/EvaluadorManos.java
+++ src/main/java/edu/badpals/pokerweb/domain/services/EvaluadorManos.java
@@ -71,7 +71,6 @@ public class EvaluadorManos {
             }
         }
 
-        // Si no se encontró un ganador, usamos la carta más alta como desempate
         if (ganadores.isEmpty()) {
             int cartaAlta = obtenerCartaAltaDeManos(evaluaciones.keySet());
             for (Jugador jugador : evaluaciones.keySet()) {
